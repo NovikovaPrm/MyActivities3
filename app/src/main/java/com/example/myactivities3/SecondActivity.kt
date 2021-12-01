6f606f4990768cbf63c59297c9aeb311c2f58ce3
NovikovaPrm <kisstep@list.ru>
2021-12-01 23:18:47 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myactivities3/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myactivities3
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
